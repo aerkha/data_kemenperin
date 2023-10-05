e6c9ca2136cc3c91ea1d77390661d9bc6fec4902
aerkha <126641782+aerkha@users.noreply.github.com>
2023-10-05 12:45:33 +0700

    Add files via upload
    mencari alamat perusahaan

---
+++ company location.py
@@ -0,0 +1,40 @@
+import googlemaps
+import pandas as pd
+
+# Gantilah 'YOUR_API_KEY' dengan kunci API Google Maps Anda yang sebenarnya
+api_key = 'AIzaSyDL9PJ7oQfz88M_KesYBy7MnLdjGl7zgg4'
+
+# Inisialisasi klien Google Maps
+gmaps = googlemaps.Client(api_key)
+
+# Membaca daftar perusahaan dari file Excel
+df = pd.read_excel('C:\\Users\\zeduna\\Documents\\Indosat\\perusahaan.xlsx')
+
+# Kolom yang berisi nama perusahaan dalam file Excel Anda
+nama_perusahaan_column = 'Nama Perusahaan'
+
+# Kolom yang akan digunakan untuk menyimpan alamat_column
+alamat_column = 'Lokasi'
+
+
+# Fungsi untuk mencari alamat perusahaan berdasarkan nama perusahaan
+def cari_alamat(nama_perusahaan):
+    try:
+        # Mencari alamat menggunakan Google Maps Geocoding API
+        hasil_geocode = gmaps.geocode(nama_perusahaan)
+
+        # Jika hasil pencarian ditemukan, ambil alamat pertama dari hasil
+        if hasil_geocode:
+            alamat = hasil_geocode[0]['formatted_address']
+            return alamat
+        else:
+            return "Alamat tidak ditemukan"
+    except Exception as e:
+        return str(e)
+
+
+# Melakukan pencarian alamat untuk setiap perusahaan dalam DataFrame
+df[alamat_column] = df[nama_perusahaan_column].apply(cari_alamat)
+
+# Menyimpan hasil pencarian alamat ke dalam file Excel
+df.to_excel('hasil_pencarian_alamat.xlsx', index=False)
